918e791af2f56b8be87a3a6f67714b83c6411ec9
AymerFi <fi.aymeric@pm.me>
2018-12-28 12:27:46 +0100

    (Server Operational): Server path big fixed

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var url = require('url');
 
 http.createServer(function(req,res) {
     var path = url.parse(req.url).pathname;
-    console.log(path.tos);
     switch(path) {
         case '/':
             res.writeHead(200, {
