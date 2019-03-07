211c0c870d22b663722102787a6c8daee3bd759a
Alex Tran <info@alex-tran.com>
2019-03-07 11:54:54 +0100

    commit before merge to master

--- anime_JS.js
+++ anime_JS.js
@@ -1,6 +1,23 @@
+// Toggle Hamburger
 function toggleMenu() {
     document.getElementById('navigation').classList.toggle("closed");
 };
 
-var x = document.getElementById('hamburgerMenu');
+var x = document.getElementById('hamburger');
 x.onclick = toggleMenu;
+
+
+// Remove closed Class 470px onward
+$(window).resize(function () {
+    if ($(window).width() > 469) {
+        $('#navigation').removeClass('closed');
+    } else {
+        $('#navigation').addClass('closed');
+    }
+});
+
+
+// Dropdown Button
+function btnFunction() {
+    document.getElementById('myDropdown').classList.toggle("show");
+};
